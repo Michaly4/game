e6625d613291ce668e1d31ae132f9d49e246ed7b
Michaly4 <ZaritskyiIvan@gmail.com>
2022-02-11 21:31:11 +0100

    game ende prep

--- app.js
+++ app.js
@@ -7,34 +7,46 @@ function getRandomInt(max) {
 }
 
 function game() {
-   //let ask = prompt("Угадай число от 1 до 100");
-   let counter = 10;
-
-   function istNummer(prm) {
-      console.log(prm);
+let counter = 10;
+   function istNummerGame(prm) {
+      //console.log(prm);
       ask = prompt("Угадай число от 1 до 100");
-      if (ask === null || counter === 0) {
+      if (ask === null) {
          alert("Игра окончена");
          counter--;
+      } else if (counter == 0) {
+         if (confirm("Попытки закончились, хотите сыграть еще?")) {
+            game();
+         } else {
+            alert("Тогда всего хорошего!🖖🏻")
+         }
       } else if (!isNumber(ask)) {
          alert("Введи число!");
          counter--;
-         istNummer(getRandomInt(100));
+         istNummerGame(getRandomInt(100));
+      } else if (ask <= 0) {
+         alert("Введи число от 1 до 100 😊 ");
+         game();
       } else if (ask < prm) {
          alert("Загаданное число меньше");
          alert(`Пробуй дальше ещё *${counter}* попыток!`);
          counter--;
-         istNummer(getRandomInt(100));
+         istNummerGame(getRandomInt(100));
       } else if (ask > prm) {
          alert("Загаданное число больше");
          alert(`Пробуй дальше ещё *${counter}* попыток!`);
          counter--;
-         istNummer(getRandomInt(100));
+         istNummerGame(getRandomInt(100));
       } else if (ask == prm) {
-         alert("Поздравляю, Вы угадали!!!");
+         alert("Поздравляю,Вы угадали!!!🏆");
+         if (confirm("Хотели бы сыграть еще?")) {
+            game();
+         } else {
+            alert("Тогда всего хорошего!🖖🏻")
+         }
       }
    }
-   istNummer(getRandomInt(100));
+   istNummerGame(getRandomInt(100));
 }
 game();
 //+++++++++++++++++++++++++++++++++++++++++++++++++++
